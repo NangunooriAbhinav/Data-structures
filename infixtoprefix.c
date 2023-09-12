932b2730472fafd86915cc0a185fd33da24d89b3
NangunooriAbhinav <131000742+NangunooriAbhinav@users.noreply.github.com>
2023-09-12 19:05:33 +0530

    fixed an error

--- infixtoprefix.c
+++ infixtoprefix.c
@@ -1,86 +1,103 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 #define SIZE 100
 
 char stack[SIZE];
 char prefix[SIZE];
-int top=-1,pretop=-1;
+int top = -1, pretop = -1;
 
 void push(char opr);
 void pop();
 int precedence(char opr);
 
-void main(){
+void main()
+{
 
     char exp[100];
     printf("enter the expression:");
-    scanf("%s",exp);
-    
-    int i=strlen(exp)-1;
+    scanf("%s", exp);
 
-    while(i>=0){
-        if(isalnum(exp[i])){
-            prefix[++pretop]=exp[i];
+    int i = strlen(exp) - 1;
+
+    while (i >= 0)
+    {
+        if (isalnum(exp[i]))
+        {
+            prefix[++pretop] = exp[i];
         }
-        else if(exp[i] == '('){
-                push(exp[i]);
+        else if (exp[i] == ')')
+        {
+            push(exp[i]);
+        }
+        else if (exp[i] == '(')
+        {
+            while (stack[top] != ')')
+            {
+                pop();
             }
-        else if(exp[i] == ')'){
-                while(stack[top] != '('){
-                    pop();
-                }
-                top--;
-            }
-        else{
-            while(precedence(stack[top]) > precedence(exp[i])){
+            top--;
+        }
+        else
+        {
+            while (precedence(stack[top]) > precedence(exp[i]))
+            {
                 pop();
             }
             push(exp[i]);
-        }        
+        }
         i--;
     }
 
-    while( top != -1){
+    while (top != -1)
+    {
         pop();
     }
 
-    printf("%s",strrev(prefix));
+    printf("%s", strrev(prefix));
 }
 
-void push(char opr){
-    if(top == SIZE-1){
-        printf("Stack overflow");   
+void push(char opr)
+{
+    if (top == SIZE - 1)
+    {
+        printf("Stack overflow");
     }
-    else{
-        stack[++top]=opr;
+    else
+    {
+        stack[++top] = opr;
     }
 }
 
-void pop(){
-    if(top == -1){
+void pop()
+{
+    if (top == -1)
+    {
         printf("stack is empty");
         exit(1);
     }
-    else{
+    else
+    {
         prefix[++pretop] = stack[top--];
     }
 }
 
-int precedence(char opr){
-    switch(opr){
-        case '+':
-        case '-':
-                 return 1;
-                 break;
-        case '*':
-        case '/':
-                 return 2;
-                 break;
-        case '^':
-                return 3;   
-        default:
-                 return 0;
+int precedence(char opr)
+{
+    switch (opr)
+    {
+    case '+':
+    case '-':
+        return 1;
+        break;
+    case '*':
+    case '/':
+        return 2;
+        break;
+    case '^':
+        return 3;
+    default:
+        return 0;
     }
 }
